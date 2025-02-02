93a59196a6fb469e7ed02fae0ed12990d323ba70
Utkarsh <utkjaiswal58@gmail.com>
2025-02-02 15:20:38 +0530

    other functionalities added

--- question_manager.py
+++ question_manager.py
@@ -7,9 +7,10 @@ from comment import Comment
 class QuestionManager:
 
     def __init__(self):
-        self.questions: Dict[str:List[Question]] = {}
-        self.answers: List[Answer] = []
-        self.comments: List[Comment] = []
+        self.questions: Dict[str:Question] = {}
+        self.answers: Dict[str: List[Answer]] = {}
+        self.comments: Dict[str:List[Comment]] = {}
+        self.user_questions: Dict[str: List[str]] = {}
 
         self.tag_index: Dict[str: Set[str]] = {}
         self.keyword_index: Dict[str: Set[str]] = {}
@@ -29,6 +30,40 @@ class QuestionManager:
                     questions.append(self.questions[self.keyword_index[x]])
         
         return questions
+    
+    def search_question_by_user(self, user_id):
+        questions = []
+
+        user_question_ids = self.user_questions[user_id]
+
+        for question_id in user_question_ids:
+            questions.append(self.questions[question_id])
+
+        return questions
+
+    
+    def question_details(self, question_id):
+        question = self.questions[question_id]
+        question_comments = []
+        for x in self.comments:
+            if x == question_id:
+                question_comments.append(self.comments[x])
+
+        answers = []
+
+        for x in self.answers:
+            if x == question_id:
+                answers.append({
+                    "answer": self.answers[x],
+                    "_answer_comments": self.comments[self.answers[x]["id"]]
+                })
+
+        return {
+            "question": question,
+            "question_comments": question_comments,
+            "answers": answers
+        }
+
 
  
     def post_question(self, question:str, keywords:List[str], tags: List[str], posted_by: str):
@@ -42,17 +77,47 @@ class QuestionManager:
         for keyword in keywords:
             self.keyword_index[keyword].add(question_id)
 
+        self.user_questions[posted_by].append(question_id)
+
         return question_obj
     
 
     def answer_question(self, question_id: str, answer: str, answered_by: str):
         answer_id = str(uuid.uuid4)
         answer = Answer(answer_id, question_id, answer, answered_by)
-        self.answers.append(answer)
+        self.answers[question_id].append(answer)
         return answer
     
     def add_comment(self, type:str, type_id: str, comment: str, commented_by: str):
         comment_id = str(uuid.uuid4)
         comment_obj = Comment(comment_id, type, type_id, comment, commented_by)
+        self.comments[comment_id].append(comment_obj)
+
         return comment_obj
     
+
+    def vote_question(self, question_id, user_id):
+        isUpvote = True
+        if user_id in self.questions[question_id]["reputation"]:
+            self.questions[question_id]["reputation"].remove(user_id)
+            isUpvote = False
+
+        else:
+            self.questions[question_id]["reputation"].add(user_id)
+
+        return isUpvote
+    
+    def vote_answer(self, answer_id, user_id):
+        isUpvote = True
+        if user_id in self.questions[answer_id]["reputation"]:
+            self.questions[answer_id]["reputation"].remove(user_id)
+            isUpvote = False
+
+        else:
+            self.questions[answer_id]["reputation"].add(user_id)
+
+        return isUpvote
+
+
+
+
